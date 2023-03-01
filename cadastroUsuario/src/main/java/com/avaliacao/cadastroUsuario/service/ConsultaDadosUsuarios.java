d825e7dacf14bb8463e31178a9c0359ac0ba749b
Leonardo Machado Fernandes <51767088+leomachandes@users.noreply.github.com>
2023-03-01 14:27:38 -0300

    Update ConsultaDadosUsuarios.java

--- cadastroUsuario/src/main/java/com/avaliacao/cadastroUsuario/service/ConsultaDadosUsuarios.java
+++ cadastroUsuario/src/main/java/com/avaliacao/cadastroUsuario/service/ConsultaDadosUsuarios.java
@@ -2,10 +2,14 @@ package com.avaliacao.cadastroUsuario.service;
 
 
 import com.avaliacao.cadastroUsuario.controller.dto.UsuarioDtoSaida;
+import com.avaliacao.cadastroUsuario.exception.UsuarioNaoEncontradoException;
+
+import java.io.IOException;
 
 public interface ConsultaDadosUsuarios {
 
     UsuarioDtoSaida findById(Long id) throws Exception;
 
 
+    byte[] buscarFotoUsuario(Long id) throws UsuarioNaoEncontradoException, IOException;
 }
